6d01fe8eb0f4a97539e0df56d8766610aae13151
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-07-27 21:54:10 -0300

    criação de exemplos usando componentes do tkinter

--- app/views/exemplo_tkinter.py
+++ app/views/exemplo_tkinter.py
@@ -1,4 +1,111 @@
-#isso aqui é pros bobo vê e bate palma
-# 
-def teste():
-    return 10 
+import tkinter as tk
+from tkinter import messagebox
+
+janela = tk.Tk()
+
+janela.title("Meu primeiro sisteminha")
+janela.geometry("800x600")
+janela.resizable(False, False)
+
+lbl_titulo = tk.Label(
+    janela,
+    text = "EXEMPLO DE CADASTRO",
+    font= ("Arial",12,"bold")
+)
+lbl_titulo.grid(
+    row = 0,
+    column = 0,
+    padx = 10,
+    pady = 5,
+    columnspan = 3
+)
+
+
+lbl_nome = tk.Label(
+    janela,
+    text = "Nome:"
+)
+lbl_nome.grid(
+    row = 1,
+    column = 0,
+    padx = 10,
+    pady = 5
+)
+txt_nome = tk.Entry(
+    janela,
+    width = 40
+)
+txt_nome.grid(
+    row = 1,
+    column = 1
+)
+
+
+lbl_idade = tk.Label(
+    janela,
+    text = "Idade"
+)
+lbl_idade.grid(
+    row = 2,
+    column = 0,
+    padx = 10,
+    pady = 5    
+)
+txt_idade = tk.Entry(
+    janela,
+    width= 40
+)
+txt_idade.grid(
+    row = 2,
+    column = 1
+)
+
+def printar():
+    print(txt_nome.get())
+
+btn_escrever_nome = tk.Button(
+    janela,
+    text = "Printar o nome",
+    command = printar
+)
+
+btn_escrever_nome.grid(
+    row = 3,
+    column = 0,
+    padx = 10,
+    pady = 5
+)
+
+def avaliar_idade():
+    if txt_idade.get() == "":
+        messagebox.showerror(
+            "Sisteminha",
+            "Tu só pode estar de sacanagem!"
+        )
+        return    
+    idade = int(txt_idade.get())
+    if idade >= 18:
+        messagebox.showinfo(
+            "Sisteminha",
+            "Com " + str(idade) + " você é bem vindo"
+        )
+        return
+    messagebox.showwarning(
+        "Sisteminha",
+        "Fedelho!!!!"
+    )
+    return
+    
+
+btn_avaliar_idade = tk.Button(
+    janela,
+    text = "Avaliar idade",
+    command = avaliar_idade
+)
+btn_avaliar_idade.grid(
+    row = 3,
+    column = 1
+)
+
+janela.mainloop()
+
